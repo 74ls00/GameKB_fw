b8d698a3244da535236b97f70614f3bf3a062c9b
74ls00 <userled@yandex.ru>
2017-01-19 22:01:28 +0400

    backup

--- EasyAVR/RunPortable.bat
+++ EasyAVR/RunPortable.bat
@@ -1,5 +1,5 @@
 title EasyAVR KeyMapper PortablePython
-set path=%path%;%~d0%~p0..\PortablePython;%path%
+set path=%path%;%~d0%~p0Python2P;%path%
 cd keymapper
 start PythonW-Portable.exe -m easykeymap.gui
 rem @pause
--- EasyAVR/run_log_port.bat
+++ EasyAVR/run_log_port.bat
@@ -1,4 +1,4 @@
-@set path=%path%;G:\home\Documents\Projects\EAGLE\Keyboard\firmware\PortablePython\App;%path%
+@set path=%path%;G:\home\Documents\Projects\EAGLE\Keyboard\firmware\EasyAVR\Python2P\App;%path%
 
 cd keymapper
 python -m easykeymap.gui
